609363e32d58c06a4969c5128aded3d97a4d134b
szaszbeni <71766125+szaszbeni@users.noreply.github.com>
2025-03-21 09:29:53 +0100

    Delete src directory

--- src/components/Contact.jsx
+++
@@ -1,9 +0,0 @@
-import { NavLink } from "react-router";
-import contact from "../components/Navigation.jsx"
-import { useState } from "react";
-export default function Navigation() {
-    let [menuKinyitva, setMenuKinyitva] = useState(false)
-    return(
-        <h1>aaaaaaaaaaaaaaa </h1>
-    )
-}
